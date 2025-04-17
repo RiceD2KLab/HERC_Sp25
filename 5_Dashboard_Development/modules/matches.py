e15d0c642849dbc52fef7bc3437a748869c99d63
Bianca Schutz <mbs5@rice.edu>
2025-04-16 22:12:41 -0500

    Modularized/organized matches page

--- 5_Dashboard_Development/modules/matches.py
+++ 5_Dashboard_Development/modules/matches.py
@@ -1,6 +1,5 @@
 from shiny import ui, render, module
-from utils.dashboardVisuals import plot_texas_districts
-from utils.helper import title_case_with_spaces
+from utils.matchUtils import plot_texas_districts, generate_table
 import pandas as pd
 
 @module.ui
@@ -27,22 +26,14 @@ def match_server(input, output, session, run_result, get_inputs):
         if get_inputs():
             return ui.p("The model identified the following districts as being the most similar to yours based on the inputs you selected.")
         else:
-            return ui.NULL  # Do not show the link if the condition is not met
+            return ui.NULL 
     @output()
     @render.data_frame
     def results_df():
         result = run_result.get()
         if result is None or len(result) != 3:
             return render.DataGrid(pd.DataFrame({"District": ['Waiting for model results. Run a model to view neighbors.']}))
-        neighbor_names = result[2]['DISTNAME']
-        print("Rendering matches table...")
-
-        df = result[0][['DISTNAME', 'TEA Description', 'CNTYNAME']]
-        df.columns = ['District', 'TEA District Type', 'County']
-        for_table = df[df['District'].isin(neighbor_names)].copy()
-
-        for_table['District'] = [title_case_with_spaces(distname) for distname in for_table['District']]
-        for_table['County'] = [title_case_with_spaces(cty) for cty in for_table['County']]
+        for_table = generate_table(result[2], result[0])
         return render.DataGrid(for_table, width = '100%')
 
     @output()
